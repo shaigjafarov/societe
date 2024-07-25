9b852c38072700196a1885b9c3dff9da5d653c16
Your Name <you@example.com>
2024-07-25 22:01:36 +0400

    add

---
+++ src/Hashmap/Main.java
@@ -0,0 +1,22 @@
+package Hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class Main {
+      public static   Map<String, Integer> getCount(String text) {
+            String[] words = text.split(" ");
+            HashMap<String, Integer> map = new HashMap<>();
+            for (String word : words) {
+                if (map.containsKey(word)) {
+                    Integer say = map.get(word);
+                    map.put(word, ++say);
+                } else
+                    map.put(word, 0);
+            }
+            return map;
+    }
+
+}
